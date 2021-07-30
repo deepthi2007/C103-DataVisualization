2b62f48d8397f1a6463142afa2cf31e747ffaf38
sai deepthi <62826707+deepthi2007@users.noreply.github.com>
2021-07-30 20:07:33 +0530

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -3,5 +3,5 @@ import plotly.express as px
 
 df = pd.read_csv("D:/deepthi projects/C103/data.csv")
 print(df)
-fig = px.scatter(df,x="date",y="cases",color="country",)
+fig = px.scatter(df,x=df["date"],y=df["cases"],color=df["country"])
 fig.show()
